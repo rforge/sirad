a48a84fe5ff352930ac951c25888e238c82cc2e0
jedboy <jedboy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-06 15:31:24 +0000

    to options added to 'cst' function: (1) option to provide extraterrestrial radiation, (2) option to choose between median and max for calculating final cst value
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sirad@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cst.R
+++ pkg/R/cst.R
@@ -1,32 +1,29 @@
-cst <-
-function(RefRad,days,lat,perce=3,sepYear=FALSE) {
- i <- dayOfYear(days) 
- 
- if (sepYear == TRUE) {
- 
- years <- unique(format(as.Date(days,origin="1970-01-01"),'%Y'))
+cst <- 
+function (RefRad, days, lat, extraT=NULL, perce = 3, sepYear = FALSE, stat="median") 
+{
+    i <- dayOfYear(days)
+    if (is.null(extraT)) extraT <- extrat(i, lat)$ExtraTerrestrialSolarRadiationDaily  
+    if (sepYear == TRUE) {
+        years <- unique(format(as.Date(days, origin = "1970-01-01"), 
+            "%Y"))
+        mtalsALL <- vector()
+        for (y in years) {
+            wh <- which(format(as.Date(days, origin = "1970-01-01"), 
+                "%Y") == y)
+            tals <- RefRad[wh]/extraT[wh]
+            stals <- sort(tals)
+            ll <- length(stals)
+            mtals <- stals[(ll - round(ll * (perce/100))):ll]
+            mtalsALL <- c(mtalsALL, mtals)
+        }
+    }
+    if (sepYear == FALSE) {
+        tals <- RefRad/extraT
+        ll <- length(sort(tals))
+        mtalsALL <- sort(tals)[(ll - round(ll * (perce/100))):ll]
 
- 
- mtalsALL <- vector()
- ex <- extrat(i,lat)$ExtraTerrestrialSolarRadiationDaily
- for (y in years) {
- wh <- which(format(as.Date(days,origin="1970-01-01"),'%Y')==y)
- tals <- RefRad[wh]/ex[wh]
- stals <- sort(tals)
- 
- ll <- length(stals)
- mtals <- stals[(ll-round(ll*(perce/100))):ll]
- mtalsALL <- c(mtalsALL,mtals)
- }
- rval <- median(mtalsALL)
- }
- 
- if (sepYear == FALSE) {
- tals <- RefRad/extrat(i,lat)$ExtraTerrestrialSolarRadiationDaily
- ll <- length(sort(tals))
- mtals <- sort(tals)[(ll-round(ll*(perce/100))):ll]
- rval <- median(mtals)
- }
- rval
+    }
+    if (stat=="median") rval <- median(mtals)
+    if (stat=="max") rval <- max(mtals)
+    rval
 }
-
