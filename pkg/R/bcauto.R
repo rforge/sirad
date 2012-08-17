04469dbfe880c209867936d436411be8341625a3
jedboy <jedboy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-17 14:21:24 +0000

    small bugs fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sirad@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bcauto.R
+++ pkg/R/bcauto.R
@@ -2,7 +2,8 @@ bcauto <-
 function (lat, lon, days, Tmax, Tmin, tal, BCb_guess = 0.13, epsilon = 0.5, 
     perce = NA, dcoast = NA) 
 {
-    if (is.na(perce)) { 
+    if (is.na(perce)) {
+    data(CFC,verbose=F) 
     p <- extract(CFC, matrix(c(lon,lat),1,2))
     perce <- -68*log(p)+0.92*p+225
     if (perce > 30) perce <- 30
