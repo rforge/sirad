8aff01407bdac05cbb7ee78d358789d8431be440
jedboy <jedboy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-13 21:51:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sirad@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ap.R
+++ pkg/R/ap.R
@@ -1,8 +1,12 @@
 ap <-
-  function(days,lat,extraT=NULL,A,B,SSD) {
+  function(days, lat, lon, extraT=NULL, A=NA, B=NA, SSD) {
     i <- dayOfYear(days)
     latt <- radians(lat)
     if (is.null(extraT)) extraT <- extrat(i=i,lat=latt)$ExtraTerrestrialSolarRadiationDaily
+    
+    if (is.na(A)) A <- extract(APa_map, matrix(c(lon,lat),1,2))
+    if (is.na(B)) B <- extract(APb_map, matrix(c(lon,lat),1,2))
+    
     ap <- extraT*(A+B*(SSD/dayLength(lat=latt,i=i)))
     ap 
   }
--- pkg/R/ha.R
+++ pkg/R/ha.R
@@ -1,8 +1,12 @@
 ha <-
-  function(days,lat,extraT=NULL,A,B,Tmax,Tmin) {
+  function(days,lat,lon,extraT=NULL,A,B,Tmax,Tmin) {
     i <- dayOfYear(days)
     latt <- radians(lat)
     if (is.null(extraT)) extraT <- extrat(i=i,lat=latt)$ExtraTerrestrialSolarRadiationDaily
+    
+    if (is.na(A)) A <- extract(Ha_map, matrix(c(lon,lat),1,2))
+    if (is.na(B)) B <- extract(Hb_map, matrix(c(lon,lat),1,2))
+    
     ha <- extraT*A*sqrt(Tmax-Tmin)+B
     ha 
   }
